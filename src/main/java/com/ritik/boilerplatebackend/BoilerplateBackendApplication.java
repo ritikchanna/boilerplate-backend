94c9ce01c5d4e0dc00d06c8b5c5ce68015865ead
Ritik Channa <b0224517@LTB0224517-Mac.local>
2021-06-02 21:39:04 +0530

    Intial Commit

---
+++ src/main/java/com/ritik/boilerplatebackend/BoilerplateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ritik.boilerplatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerplateBackendApplication.class, args);
+	}
+
+}
